977058b1aeaf7c32616dbda2c902090a4e7066a9
Ravi kumar <1002kumarravi@gmail.com>
2023-12-01 09:54:59 +0530

    setup mui and tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
 
   return (
     <>
-      <h1>Home Page</h1>
+      <h1 className='text-green-700'>Home Page</h1>
+      <Button variant="contained">Contained</Button>
     </>
   )
 }
