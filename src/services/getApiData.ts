8d470d98b936caf56fbd74f3c5c40d006afb81cc
Ravi kumar <1002kumarravi@gmail.com>
2023-12-06 20:01:07 +0530

    add api funtion to get access token

--- src/services/getApiData.ts
+++ src/services/getApiData.ts
@@ -1,48 +1,55 @@
-import appConfigs from '@/config/appConfigs';
-import spotifyAxiosInstance from './axiosInstance/spotify';
-import axios, { AxiosRequestConfig } from 'axios';
+import appConfigs from '../config/appConfigs';
+// import spotifyAxiosInstance from './axiosInstance/spotify';
+import axios from 'axios';
 
-const { client_secret, client_id } = appConfigs.spotify;
+const { client_id, client_secret } = appConfigs.spotify;
 
-const getSpotifyAccessToken = async () => {
-    const token = localStorage.getItem('spotifyAccessToken')
-    if (token) return token
+export const getSpotifyAccessToken = async () => {
+    const tokenexpiresIn = localStorage.getItem('spotifyAccesstokenexpiresIn')
+    const spotifyAccessToken = localStorage.getItem('spotifyAccessToken')
 
+    console.log(client_id, client_secret)
+    const currentTime = Math.floor(Date.now() / 1000)
+    if (currentTime && Number(tokenexpiresIn) > Number(currentTime)) return spotifyAccessToken
+    const enc = `${client_id}:${client_secret}`.toString('base64');
+    console.log(enc)
     const authOptions = {
         url: 'https://accounts.spotify.com/api/token',
         method: 'post',
         headers: {
-            'Authorization': 'Basic ' + atob(client_id + ':' + client_secret),
             'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${btoa(enc)}`,
         },
         data: 'grant_type=client_credentials',
     };
 
     const result = await axios(authOptions)
         .then(response => {
-            const token = response.data.access_token;
+            const token = response.data;
+            localStorage.setItem("spotifyAccessToken", token.access_token);
+            localStorage.setItem("spotifyAccesstokenexpiresIn", `${Math.floor(Date.now() / 1000) + 3600}`);
+
             console.log('Access Token:', token);
         })
         .catch(error => {
             console.error('Error getting access token:', error.response ? error.response.data : error.message);
         });
-
-    console.log(result)
 }
 
-export const GetSpotifyDataAsJSON = async<TResult = unknown>(
-    url: string,
-    config: AxiosRequestConfig = {}
-): Promise<TResult> => {
-    console.log(url, config, "get api data")
-    const token = await getSpotifyAccessToken()
-    const response = await spotifyAxiosInstance.get<TResult>(url, {
-        ...config,
-        headers: {
-            'Content-Type': 'application/json',
-            ...config.headers,
-        },
-    });
-    return response.data;
-}
+// export const GetSpotifyDataAsJSON = async<TResult = unknown>(
+//     url: string,
+//     config: AxiosRequestConfig = {}
+// ): Promise<TResult> => {
+//     console.log(url, config, "get api data")
+//     const token = await getSpotifyAccessToken()
+//     const response = await spotifyAxiosInstance.get<TResult>(url, {
+//         ...config,
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization' : Bearer
+//             ...config.headers,
+//         },
+//     });
+//     return response.data;
+// }
 
