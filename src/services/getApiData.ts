ad9a5de00fe946e28516184cd4288cf01ffe310b
Ravi kumar <1002kumarravi@gmail.com>
2023-12-07 22:01:25 +0530

    add api handled

--- src/services/getApiData.ts
+++ src/services/getApiData.ts
@@ -1,6 +1,6 @@
 import appConfigs from '../config/appConfigs';
-// import spotifyAxiosInstance from './axiosInstance/spotify';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import spotifyAxiosInstance from './axiosInstance/spotify';
 
 const { client_id, client_secret } = appConfigs.spotify;
 
@@ -12,7 +12,7 @@ export const getSpotifyAccessToken = async () => {
     const currentTime = Math.floor(Date.now() / 1000)
     if (currentTime && Number(tokenexpiresIn) > Number(currentTime)) return spotifyAccessToken
     const enc = `${client_id}:${client_secret}`.toString('base64');
-    console.log(enc)
+    let accessToken;
     const authOptions = {
         url: 'https://accounts.spotify.com/api/token',
         method: 'post',
@@ -23,33 +23,33 @@ export const getSpotifyAccessToken = async () => {
         data: 'grant_type=client_credentials',
     };
 
-    const result = await axios(authOptions)
+    await axios(authOptions)
         .then(response => {
             const token = response.data;
             localStorage.setItem("spotifyAccessToken", token.access_token);
             localStorage.setItem("spotifyAccesstokenexpiresIn", `${Math.floor(Date.now() / 1000) + 3600}`);
-
-            console.log('Access Token:', token);
+            accessToken = token.access_token
         })
         .catch(error => {
             console.error('Error getting access token:', error.response ? error.response.data : error.message);
         });
+
+    return accessToken
 }
 
-// export const GetSpotifyDataAsJSON = async<TResult = unknown>(
-//     url: string,
-//     config: AxiosRequestConfig = {}
-// ): Promise<TResult> => {
-//     console.log(url, config, "get api data")
-//     const token = await getSpotifyAccessToken()
-//     const response = await spotifyAxiosInstance.get<TResult>(url, {
-//         ...config,
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization' : Bearer
-//             ...config.headers,
-//         },
-//     });
-//     return response.data;
-// }
+export const GetSpotifyDataAsJSON = async<TResult = unknown>(
+    url: string,
+    config: AxiosRequestConfig = {}
+): Promise<TResult> => {
+    const token = await getSpotifyAccessToken()
+    const response = await spotifyAxiosInstance.get<TResult>(url, {
+        ...config,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+            ...config.headers,
+        },
+    });
+    return response.data;
+}
 
