957b2e9e39e21012ccf200fa0738a0412816ef9c
Ravi kumar <1002kumarravi@gmail.com>
2023-12-17 21:45:52 +0530

    add playing song ID

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,16 +5,20 @@ interface IStore {
     audioLevel: number
     isAudioMuted: boolean
     currentTrack: number
+    playingsongId: string | null
     setAudioLevel: (audioLevel: number) => void
     setIsAudioMuted: (isAudioMuted: boolean) => void
     setCurrentTrack: (currentTrack: number) => void
+    setPlayingSongId: (playingsongId: string | null) => void
 }
 
 export const useAppStore = create<IStore>()((set) => ({
     audioLevel: 40,
     isAudioMuted: false,
     currentTrack: 0,
+    playingsongId: null,
     setAudioLevel: (audioLevel: number) => set({ audioLevel }),
     setIsAudioMuted: (isAudioMuted: boolean) => set({ isAudioMuted }),
-    setCurrentTrack: (currentTrack: number) => set({ currentTrack })
+    setCurrentTrack: (currentTrack: number) => set({ currentTrack }),
+    setPlayingSongId: (playingsongId: string | null) => set({ playingsongId })
 }));
