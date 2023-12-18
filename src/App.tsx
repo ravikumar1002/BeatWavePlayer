daf58fc7fc6784a835b052c8df1d51af8e9f41ec
Ravi kumar <1002kumarravi@gmail.com>
2023-12-19 00:09:15 +0530

    feat:playlist card

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,50 @@ import { GetSpotifyDataAsJSON } from './services/getApiData';
 import { useState, useEffect } from 'react'
 import AudioPlayer from './components/AudioPlayer/AudioPlayer'
 import { Box } from '@mui/material';
+import PlaylistCard from './components/PlaylistCard/PlaylistCard';
 
 const App = () => {
 
 
-  const [data, setData] = useState<PlaylsitDataDTO>()
+  const [data, setData] = useState()
+
+  // const getTrendingData = async () => {
+
+  //   const trendingResponse = await GetSpotifyDataAsJSON<PlaylsitDataDTO>("/playlists/37i9dQZEVXbLZ52XmnySJg", {
+  //     params: {},
+  //   });
+  //   setData(trendingResponse)
+  //   return trendingResponse
+  // }
 
   const getTrendingData = async () => {
 
-    const trendingResponse = await GetSpotifyDataAsJSON<PlaylsitDataDTO>("/playlists/37i9dQZEVXbLZ52XmnySJg", {
+    const trendingResponse = await GetSpotifyDataAsJSON<PlaylsitDataDTO>("browse/featured-playlists?limit=24", {
       params: {},
     });
     setData(trendingResponse)
     return trendingResponse
   }
 
+  // const getTrendingData = async () => {
+
+  //   const trendingResponse = await GetSpotifyDataAsJSON<PlaylsitDataDTO>("browse/categories?limit=50", {
+  //     params: {},
+  //   });
+  //   setData(trendingResponse)
+  //   return trendingResponse
+  // }
   useEffect(() => {
     getTrendingData()
   }, [])
 
-
+  console.log(data, "data")
   return (
     <>
       <Box className={"relative"}>
         <Header />
-        {data && <TreadingPage data={data} />}
-        {data && <AudioPlayer playlist={data?.tracks?.items.map((item) => {
+        {/* {data && <TreadingPage data={data} />} */}
+        {/* {data && <AudioPlayer playlist={data?.tracks?.items.map((item) => {
           return {
             title: item.track.name,
             url: item.track?.preview_url ? item.track?.preview_url : "",
@@ -40,7 +58,18 @@ const App = () => {
           }
         })}
         />
-        }
+        } */}
+        <Box className="flex flex-wrap gap-4">
+          {data && data?.playlists.items.map((item) => {
+            return (
+              <PlaylistCard details={{
+                image: item.images[0].url,
+                name: item.name,
+                description: item.description,
+              }} />
+            )
+          })}
+        </Box>
       </Box >
     </>
   )
---
+++ src/components/PlaylistCard/PlaylistCard.tsx
@@ -0,0 +1,69 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Box, CardActionArea, SxProps } from '@mui/material';
+import "./playlist-card.css"
+interface IDetailsProps {
+    image: string
+    name: string
+    description: string
+}
+
+interface IPlaylistCardProps {
+    details: IDetailsProps
+}
+
+const styles: Record<string, SxProps> = {
+    wrapper: {
+        maxWidth: 145,
+        "&:hover": {
+            "& .image-container": {
+                transform: "scale(1.1)",
+                transition: "transform .4s",
+            }
+        }
+    }
+
+
+}
+
+
+export default function PlaylistCard(props: IPlaylistCardProps) {
+    console.log(props, "props")
+    const { image, name, description } = props.details
+    return (
+        <Card sx={styles.wrapper}>
+            <CardActionArea>
+                <Box sx={{
+                    overflow: 'hidden',
+                }}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={image}
+                        alt={name}
+                        className='image-container'
+                    />
+                </Box>
+
+                <CardContent>
+                    <Typography gutterBottom variant="subtitle2" component="div" fontWeight={"800"} sx={{
+                        lineBreak: "anywhere",
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                    }}>
+                        {name}
+                    </Typography>
+                    <Box className="mydiv">
+                        <Typography variant="caption" color="text.secondary" className='mytext'>
+                            {description}
+                        </Typography>
+                        <Box color="text.secondary" className='myelli'>...</Box>
+                    </Box>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
