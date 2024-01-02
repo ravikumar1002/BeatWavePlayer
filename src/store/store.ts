2676301a9d36d3b17e7ea529bb0b83a4454432ea
Ravi kumar <1002kumarravi@gmail.com>
2024-01-02 09:59:48 +0530

    open playlist info state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,3 +1,4 @@
+import { PlaylsitDataDTO } from '../dto/playlistDataDTO';
 import { create } from 'zustand';
 
 
@@ -5,20 +6,28 @@ interface IStore {
     audioLevel: number
     isAudioMuted: boolean
     currentTrack: number
+    playlistSongs: PlaylsitDataDTO | null
     playingsongId: string | null
+    openPlaylist: PlaylsitDataDTO | null
     setAudioLevel: (audioLevel: number) => void
     setIsAudioMuted: (isAudioMuted: boolean) => void
     setCurrentTrack: (currentTrack: number) => void
+    setPlaylistSongs: (playlistSongs: PlaylsitDataDTO | null) => void
     setPlayingSongId: (playingsongId: string | null) => void
+    setOpenPlaylist: (openPlaylist: PlaylsitDataDTO | null) => void
 }
 
 export const useAppStore = create<IStore>()((set) => ({
     audioLevel: 40,
     isAudioMuted: false,
     currentTrack: 0,
+    playlistSongs: null,
     playingsongId: null,
+    openPlaylist: null,
     setAudioLevel: (audioLevel: number) => set({ audioLevel }),
     setIsAudioMuted: (isAudioMuted: boolean) => set({ isAudioMuted }),
     setCurrentTrack: (currentTrack: number) => set({ currentTrack }),
-    setPlayingSongId: (playingsongId: string | null) => set({ playingsongId })
+    setPlaylistSongs: (playlistSongs: PlaylsitDataDTO | null) => set({ playlistSongs }),
+    setPlayingSongId: (playingsongId: string | null) => set({ playingsongId }),
+    setOpenPlaylist: (openPlaylist: PlaylsitDataDTO | null) => set({ openPlaylist }),
 }));
