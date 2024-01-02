83a3edb721db5989907a7f753c34b5c00e0548e6
Ravi kumar <1002kumarravi@gmail.com>
2024-01-02 09:59:25 +0530

    route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,49 @@
 import './App.css'
-import Header from './components/Header/Header';
-import { PlaylsitDataDTO } from './dto/playlistDataDTO';
 import TreadingPage from './pages/TrendingPage';
-import { GetSpotifyDataAsJSON } from './services/getApiData';
-import { useState, useEffect } from 'react'
-import AudioPlayer from './components/AudioPlayer/AudioPlayer'
+
 import { Box } from '@mui/material';
-import PlaylistCard from './components/PlaylistCard/PlaylistCard';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from './pages/HomePage';
-import DetailsPage from './pages/DetailsPAge';
+import DetailsPage from './pages/DetailsPage';
+import { PageWrapper } from './components/PageWrapper';
 
 const App = () => {
-
-
-  const [data, setData] = useState()
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: (
-        <HomePage />
+        <PageWrapper>
+          <HomePage />
+        </PageWrapper>
       ),
     },
     {
       path: "/playlist",
       element: (
-        <DetailsPage />
+        <PageWrapper>
+          <DetailsPage />
+        </PageWrapper>
       )
-    }
-
+    },
+    {
+      path: "/playlist/:playlistid",
+      element: (
+        <PageWrapper>
+          <TreadingPage />
+        </PageWrapper>
+      ),
+    },
   ]);
 
-  // const getTrendingData = async () => {
-
-  //   const trendingResponse = await GetSpotifyDataAsJSON<PlaylsitDataDTO>("/playlists/37i9dQZEVXbLZ52XmnySJg", {
-  //     params: {},
-  //   });
-  //   setData(trendingResponse)
-  //   return trendingResponse
-  // }
-
-  const getTrendingData = async () => {
-
-    const trendingResponse = await GetSpotifyDataAsJSON<PlaylsitDataDTO>("browse/featured-playlists?limit=24", {
-      params: {},
-    });
-    setData(trendingResponse)
-    return trendingResponse
-  }
-
-  // const getTrendingData = async () => {
-
-  //   const trendingResponse = await GetSpotifyDataAsJSON<PlaylsitDataDTO>("browse/categories?limit=50", {
-  //     params: {},
-  //   });
-  //   setData(trendingResponse)
-  //   return trendingResponse
-  // }
-  useEffect(() => {
-    getTrendingData()
-  }, [])
-
-  console.log(data, "data")
   return (
     <>
       <RouterProvider router={router} />
       <Box className={"relative"}>
-        <Header />
+        {/* <Header /> */}
         {/* {data && <TreadingPage data={data} />} */}
-        {/* {data && <AudioPlayer playlist={data?.tracks?.items.map((item) => {
-          return {
-            title: item.track.name,
-            url: item.track?.preview_url ? item.track?.preview_url : "",
-            image: item.track.album.images[0].url,
-            id: item.track.id
-          }
-        })}
-        />
-        } */}
-        <Box className="flex flex-wrap gap-4">
-          {data && data?.playlists.items.map((item) => {
-            return (
-              <PlaylistCard details={{
-                image: item.images[0].url,
-                name: item.name,
-                description: item.description,
-              }} />
-            )
-          })}
-        </Box>
+
+
       </Box >
     </>
   )
