13795dfde73fe7a4666427241a18b0f57b75a4e7
Ravi kumar <1002kumarravi@gmail.com>
2024-01-14 17:10:34 +0530

    add: elevationScroll for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,38 +4,61 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { useNavigate } from 'react-router';
+import { useScrollTrigger } from '@mui/material';
+import { cloneElement } from 'react';
+
+interface Props {
+    window?: () => Window;
+    children: React.ReactElement;
+  }
+  
+
+function ElevationScroll(props: Props) {
+    const { children, window } = props;
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+        target: window ? window() : undefined,
+    });
+
+    return cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
 
 const Header = () => {
     const navigate = useNavigate()
     return (
-        <AppBar position="static" sx={{
-            background: "white",
-            color: "purple",
-            filter: "drop-shadow(rgba(0, 0, 0, 0.08) 0px 4px 16px)",
-        }}>
-            <Container maxWidth="xl">
-                <Toolbar disableGutters className='p-6'>
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="#"
-                        sx={{
-                            mr: 2,
-                            fontWeight: 700,
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                        onClick={() => {
-                            navigate("/")
-                        }}
-                        data-testid="pageTitle"
-                    >
-                        Yfitops
-                    </Typography>
-                </Toolbar>
-            </Container>
-        </AppBar>
+        <ElevationScroll>
+            <AppBar position="sticky" elevation={3} sx={{
+                background: "white",
+                color: "purple",
+            }}>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters className='p-6'>
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href="#"
+                            sx={{
+                                mr: 2,
+                                fontWeight: 700,
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                            onClick={() => {
+                                navigate("/")
+                            }}
+                            data-testid="pageTitle"
+                        >
+                            Yfitops
+                        </Typography>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ElevationScroll>
     )
 }
 
