5eb03c3d71e1adcfde984bfeaf954b8575faeb33
Ravi kumar <1002kumarravi@gmail.com>
2024-01-14 17:18:38 +0530

    code refactor

--- src/components/CategorySection/CategorySection.tsx
+++ src/components/CategorySection/CategorySection.tsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material"
-import PlaylistCard from "../PlaylistCard/PlaylistCard"
 import { useNavigate } from "react-router"
+import PlaylistCard from "../PlaylistCard/PlaylistCard"
 import { CategoryDTO } from "../../dto/categoryDTO"
 
 interface ICategorySection {
@@ -12,25 +12,23 @@ export const CategorySection = (props: ICategorySection) => {
     const { title, data } = props
     const navigate = useNavigate();
 
+    const playlistItems = data?.playlists?.items || []
+
     return (
         <Box className="p-6">
             <Box className="mb-5">
                 <Typography variant="h4" component="div">{title}</Typography>
             </Box>
             <Box className="flex flex-wrap gap-4">
-                {data && data?.playlists.items.map((item) => {
-                    return (
-                        <div key={item.id} onClick={() => {
-                            navigate(`/playlist/${item.id}`)
-                        }}>
-                            <PlaylistCard details={{
-                                image: item.images[0].url,
-                                name: item.name,
-                                description: item.description,
-                            }} />
-                        </div>
-                    )
-                })}
+                {playlistItems.map((item) => <div key={item.id} onClick={() => {
+                    navigate(`/playlist/${item.id}`)
+                }}>
+                    <PlaylistCard details={{
+                        image: item.images[0].url,
+                        name: item.name,
+                        description: item.description,
+                    }} />
+                </div>)}
             </Box>
         </Box>
     )
