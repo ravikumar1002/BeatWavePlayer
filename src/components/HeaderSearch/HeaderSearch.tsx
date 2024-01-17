e4af0ab009b39732a722f584b5956760bc9d1541
Ravi kumar <1002kumarravi@gmail.com>
2024-01-17 11:19:18 +0530

    feat: added search component

--- src/components/HeaderSearch/HeaderSearch.tsx
+++ src/components/HeaderSearch/HeaderSearch.tsx
@@ -14,7 +14,6 @@ const Search = styled('div')(({ theme }) => ({
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: '100%',
-    display:"flex",
     [theme.breakpoints.up('sm')]: {
         marginLeft: theme.spacing(3),
         width: 'auto',
