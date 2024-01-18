e6b7d6c96fe8ce853c51acec0bf5b167de58d538
Ravi kumar <1002kumarravi@gmail.com>
2024-01-18 20:38:29 +0530

    search suggestion UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,8 @@ import {
 import { cloneElement, useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import { useDebounce } from "@hooks/useDebounce";
-import { spotifySearchApi } from "@hooks/useSpotifySearchApi";
+import { spotifySearchApi } from "@hooks/spotifySearchApi";
+import { data } from "./data";
 
 interface Props {
   window?: () => Window;
@@ -36,11 +37,23 @@ function ElevationScroll(props: Props) {
 
 export const Header = () => {
   const navigate = useNavigate();
-  const [searchString, setSearchString] = useState(" ")
-
-  const suggestionSearchhandler = useDebounce(() => spotifySearchApi(searchString));
+  const [searchString, setSearchString] = useState<string>("")
+  const [showSearchSuggestion, setShowSearchSuggestion] = useState<boolean>(false)
+  const suggestionSearchhandler = useDebounce(() => spotifySearchApi(searchString, 5));
   suggestionSearchhandler()
 
+  const dataAssemble = [...data.albums.items, ...data.artists.items, ...data.playlists.items, ...data.tracks.items]
+
+  const showSuggestionFn = (searchLength: number) => {
+    if (searchLength > 0) {
+      setShowSearchSuggestion(true);
+    } else {
+      setShowSearchSuggestion(false);
+    }
+  }
+
+
+  console.log(dataAssemble)
   return (
     <ElevationScroll>
       <AppBar
@@ -52,7 +65,7 @@ export const Header = () => {
         }}
       >
         <Container maxWidth="xl">
-          <Toolbar disableGutters className="p-6" sx={{
+          <Toolbar disableGutters className="p-3" sx={{
             width: "100%",
           }}>
             <Box>
@@ -75,23 +88,72 @@ export const Header = () => {
                 Yfitops
               </Typography>
             </Box>
-            <Box>
-              <Paper
-                component="form"
-                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250 }}
+            <Box sx={{
+              flexGrow: 1,
+              flexShrink: 1,
+              display: "flex",
+              justifyContent: "center",
+            }}>
+              <Box
               >
-                <InputBase
-                  sx={{ ml: 1, flex: 1 }}
-                  placeholder="Search"
-                  inputProps={{ 'aria-label': 'search' }}
-                  onChange={(e) => {
-                    setSearchString(e.target.value)
-                  }}
-                />
-                <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                  <SearchIcon />
-                </IconButton>
-              </Paper>
+                <Paper
+                  component="div"
+                  sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 600, position: "relative" }}
+                >
+                  <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Search"
+                    inputProps={{ 'aria-label': 'search' }}
+                    onChange={(e) => {
+                      showSuggestionFn(e.target.value.length)
+                      setSearchString(e.target.value)
+                    }}
+                    onFocus={(e) => {
+                      showSuggestionFn(e.target.value.length)
+                    }}
+                  />
+                  <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                    <SearchIcon />
+                  </IconButton>
+                  {showSearchSuggestion && <Box sx={{
+                    position: "absolute",
+                    top: "3.5rem",
+                    borderRadius: "5px",
+                    left: 0,
+                    zIndex: "20",
+                    height: 400,
+                    overflowY: "scroll",
+                    background: "white",
+                    width: "100%"
+                  }} id="scrollBarDesign">
+                    {
+                      dataAssemble.map((item, i) => {
+                        console.log(item)
+                        return (
+                          <Box key={i} sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "1rem",
+                            padding: "0.5rem",
+                            cursor: "pointer",
+                            "&:hover": {
+                              background: "lavender"
+                            }
+                          }}>
+                            <Box>
+                              <img src={item?.images ? item?.images[0]?.url : item?.album?.images[0]?.url} alt={item?.name} width={40} height={40} style={{ borderRadius: "5px" }} />
+                            </Box>
+                            <Box>
+                              <Typography variant="body2" sx={{ fontWeight: "600" }}>{item?.name}</Typography>
+                            </Box>
+                          </Box>
+                        )
+                      })
+                    }
+                  </Box>}
+                </Paper>
+              </Box>
+
             </Box>
           </Toolbar>
         </Container>
--- src/hooks/spotifySearchApi.ts
+++ src/hooks/spotifySearchApi.ts
@@ -1,7 +1,7 @@
 import { GetSpotifyDataAsJSON } from "../services/getApiData";
 
 export const spotifySearchApi = async (searchString: string, limit: number) => {
-    const trendingResponse = await GetSpotifyDataAsJSON(`/search?q=${searchString}&type=album%2Cartist%2Cplaylist%2Ctrack&limit=5`, {
+    const trendingResponse = await GetSpotifyDataAsJSON(`/search?q=${searchString}&type=album%2Cartist%2Cplaylist%2Ctrack&limit=${limit}`, {
         params: {},
     });
     console.log(trendingResponse)
