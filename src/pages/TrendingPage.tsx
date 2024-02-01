857e4ccc6ba7431fa3de7baa7da21d2e45942c7d
Ravi kumar <1002kumarravi@gmail.com>
2024-02-01 19:03:00 +0530

    refactor code

--- src/pages/TrendingPage.tsx
+++ src/pages/TrendingPage.tsx
@@ -14,7 +14,6 @@ export const TrendingPage = () => {
   const { setPlaylistSongs, setOpenPlaylist, setCurrentTrack } = useAppStore();
   const [loadingState, setLoadingState] = useState<"loading" | "fulfilled" | "default">("default");
 
-
   const getTrendingData = async (playlistID: string) => {
     setLoadingState("loading");
     const trendingResponse = await GetSpotifyDataAsJSON<PlaylistDataDTO>(
@@ -33,6 +32,31 @@ export const TrendingPage = () => {
     getTrendingData(`${playlistid}`);
   }, [playlistid]);
 
+  const getBannerData = (playlistDetails: any) => {
+    return {
+      imageUrl: playlistDetails?.images[0].url ? playlistDetails?.images[0].url : "",
+      name: playlistDetails?.name ? playlistDetails?.name : "",
+      description: playlistDetails?.description ? playlistDetails?.description : "",
+      itemsLength: playlistDetails?.tracks.items.length ? playlistDetails?.tracks.items.length : 0,
+      followers: playlistDetails?.followers.total ? playlistDetails?.followers.total : 0,
+    };
+  };
+
+  const getTracksItemsData = (items: any) => {
+    const tracksItems = items.map((item) => {
+      return {
+        title: item.track.name,
+        url: item.track?.preview_url ? item.track?.preview_url : "",
+        image: item.track.album.images[0].url,
+        id: item.track.id,
+        artists: item.track.artists,
+        release_year: item.track.album.release_date,
+        album: item.track.album.name,
+      };
+    });
+    return tracksItems;
+  };
+
   return (
     <Box
       sx={{
@@ -41,36 +65,16 @@ export const TrendingPage = () => {
     >
       {loadingState === "fulfilled" && (
         <DetailsPageBanner
-          bannerDetails={{
-            imageUrl: playlistDetails?.images[0].url,
-            name: playlistDetails?.name,
-            description: playlistDetails?.description,
-            itemsLength: playlistDetails?.tracks.items.length,
-            followers: playlistDetails?.followers.total,
-          }}
+          bannerDetails={getBannerData(playlistDetails)}
           onClick={() => {
-            const tracksItems = playlistDetails?.tracks?.items.map((item) => {
-              return {
-                title: item.track.name,
-                url: item.track?.preview_url ? item.track?.preview_url : "",
-                image: item.track.album.images[0].url,
-                id: item.track.id,
-                artists: item.track.artists,
-                release_year: item.track.album.release_date,
-                album: item.track.album.name,
-              };
-            });
+            const tracksItems = getTracksItemsData(playlistDetails?.tracks.items);
             setPlaylistSongs(tracksItems ? tracksItems : null);
             setCurrentTrack(0);
           }}
         />
       )}
       {loadingState === "loading" && <DetailsPageBannerSkeleton />}
-      <Box
-        sx={{
-          padding: "0.5rem 2rem",
-        }}
-      >
+      <Box className="px-8 py-2">
         {loadingState === "loading" &&
           Array(20)
             .fill(0)
