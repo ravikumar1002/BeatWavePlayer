aa7960f862dd7506d3e5fb51f341870e26f6fcc1
Ravi kumar <1002kumarravi@gmail.com>
2024-02-02 20:07:54 +0530

    refactor banner details page skeleton

--- src/components/DetailsPageBanner/DetailsPageBannerSkeleton.tsx
+++ src/components/DetailsPageBanner/DetailsPageBannerSkeleton.tsx
@@ -3,14 +3,9 @@ import { Box, Skeleton } from "@mui/material";
 export const DetailsPageBannerSkeleton = () => {
   return (
     <Box className="flex items-center w-full">
-      <Box
-        sx={{
-          width: "20rem",
-          height: "25rem",
-          padding: "1rem 2rem",
-        }}
-      >
+      <Box className="w-80 h-96 px-8 py-4">
         <Skeleton
+          className="w-full h-full"
           sx={{
             width: "100%",
             height: "100%",
@@ -18,19 +13,14 @@ export const DetailsPageBannerSkeleton = () => {
         />
       </Box>
       <Box
+        className="mt-4"
         sx={{
-          marginTop: "1rem",
           flexGrow: 2,
           width: "13rem",
         }}
       >
         <Box>
-          <Skeleton
-            sx={{
-              width: "25rem",
-              height: "4rem",
-            }}
-          />
+          <Skeleton className="w-96 h-16" />
           <Skeleton
             sx={{
               width: "40rem",
@@ -38,17 +28,8 @@ export const DetailsPageBannerSkeleton = () => {
           />
         </Box>
         <Box className="flex gap-5 flex-wrap">
-          <Skeleton
-            sx={{
-              width: "5rem",
-            }}
-          />
-          <Skeleton
-            sx={{
-              width: "5rem",
-              padding: "1rem 2rem",
-            }}
-          />
+          <Skeleton className="w-20" />
+          <Skeleton className="w-20 px-8 py-4" />
         </Box>
         <Box className="ml-12 mt-4">
           <Skeleton width={90} height={60} />
