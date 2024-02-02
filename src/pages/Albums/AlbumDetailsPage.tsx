21e637bcb145b6064c627c6004ccef5aff48c6e5
Ravi kumar <1002kumarravi@gmail.com>
2024-02-02 11:36:36 +0530

    add album details page

--- src/pages/Albums/AlbumDetailsPage.tsx
+++ src/pages/Albums/AlbumDetailsPage.tsx
@@ -3,57 +3,18 @@ import { useParams } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 import { GetSpotifyDataAsJSON } from "@services/getApiData";
 import { DetailsPageBanner } from "@components/DetailsPageBanner/DetailsPageBanner";
-import { VerticalSongCard } from "@components/SongCard";
+import { SkeletonVerticalSongCard, VerticalSongCard } from "@components/SongCard";
+import { getBannerData } from "@utils/getBannerData";
+import { DetailsPageBannerSkeleton } from "@components/DetailsPageBanner/DetailsPageBannerSkeleton";
+import { getReleaseYearValue } from "@hooks/getReleaseYearValue";
+import { getTracksItemsData } from "@utils/getTracksItemsData";
+import { useAppStore } from "@store/store";
 
 export const AlbumDetailsPage = () => {
-  // const [playlistDetails, setPlaylistDetails] = useState<PlaylistDataDTO | null>(null);
   const { albumId } = useParams();
-  // const { setPlaylistSongs, setOpenPlaylist, setCurrentTrack } = useAppStore();
-  // const [loadingState, setLoadingState] = useState<"loading" | "fulfilled" | "default">("default");
+  const { setPlaylistSongs, setOpenPlaylist, setCurrentTrack } = useAppStore();
 
-  // const getTrendingData = async (albumId: string) => {
-  //   // setLoadingState("loading");
-  //   console.log(albumId);
-  //   const trendingResponse = await GetSpotifyDataAsJSON(`/albums/${albumId}`, {
-  //     params: {},
-  //   });
-  //   console.log(trendingResponse);
-  //   setPlaylistDetails(trendingResponse);
-  //   setOpenPlaylist(trendingResponse);
-  //   setLoadingState("fulfilled");
-  //   return trendingResponse;
-  // };
-
-  // useEffect(() => {
-  //   getTrendingData(`${albumId}`);
-  // }, [albumId]);
-
-  // const getBannerData = (playlistDetails: any) => {
-  //   return {
-  //     imageUrl: playlistDetails?.images[0].url ? playlistDetails?.images[0].url : "",
-  //     name: playlistDetails?.name ? playlistDetails?.name : "",
-  //     description: playlistDetails?.description ? playlistDetails?.description : "",
-  //     itemsLength: playlistDetails?.tracks.items.length ? playlistDetails?.tracks.items.length : 0,
-  //     followers: playlistDetails?.followers.total ? playlistDetails?.followers.total : 0,
-  //   };
-  // };
-
-  // const getTracksItemsData = (items: any) => {
-  //   const tracksItems = items.map((item) => {
-  //     return {
-  //       title: item.track.name,
-  //       url: item.track?.preview_url ? item.track?.preview_url : "",
-  //       image: item.track.album.images[0].url,
-  //       id: item.track.id,
-  //       artists: item.track.artists,
-  //       release_year: item.track.album.release_date,
-  //       album: item.track.album.name,
-  //     };
-  //   });
-  //   return tracksItems;
-  // };
-
-  const getgetAlbumPaginated = async (albumId: string | undefined) => {
+  const getAlbumDetails = async (albumId: string | undefined) => {
     const trendingResponse = await GetSpotifyDataAsJSON(`/albums/${albumId}`, {
       params: {},
     });
@@ -62,41 +23,60 @@ export const AlbumDetailsPage = () => {
 
   const AlbumDetailsQuery = useQuery({
     queryKey: ["albumDetails"],
-    queryFn: () => getgetAlbumPaginated(albumId),
+    queryFn: async () => {
+      const albumData = await getAlbumDetails(albumId);
+      setOpenPlaylist(albumData);
+      return albumData;
+    },
   });
 
   console.log(AlbumDetailsQuery.data, AlbumDetailsQuery.isLoading);
-
+  console.log(getBannerData(AlbumDetailsQuery.data));
   return (
     <Box
       sx={{
         background: "azure",
       }}
     >
-      {/* {AlbumDetailsQuery.isLoading && (
+      {AlbumDetailsQuery.isSuccess && (
         <DetailsPageBanner
-          bannerDetails={getBannerData(playlistDetails)}
+          bannerDetails={{
+            ...getBannerData(AlbumDetailsQuery.data),
+            subText3: getReleaseYearValue(AlbumDetailsQuery?.data?.release_date),
+            subText1: AlbumDetailsQuery.data?.artists.map((artist) => artist.name),
+          }}
           onClick={() => {
-            // const tracksItems = getTracksItemsData(playlistDetails?.tracks.items);
-            // setPlaylistSongs(tracksItems ? tracksItems : null);
-            // setCurrentTrack(0);
+            const tracksItems = getTracksItemsData(AlbumDetailsQuery.data?.tracks.items);
+            setPlaylistSongs(tracksItems ? tracksItems : null);
+            setCurrentTrack(0);
           }}
         />
-      )} */}
-      {/* {loadingState === "loading" && <DetailsPageBannerSkeleton />}
+      )}
+      {AlbumDetailsQuery.isLoading && <DetailsPageBannerSkeleton />}
+
       <Box className="px-8 py-2">
-        {loadingState === "loading" &&
+        {AlbumDetailsQuery.isLoading &&
           Array(20)
             .fill(0)
             .map((_, i) => {
               return <SkeletonVerticalSongCard key={i} />;
             })}
-        {loadingState === "fulfilled" &&
-          playlistDetails &&
-          playlistDetails?.tracks?.items.map((item, i) => {
-            return <VerticalSongCard key={i} songDetails={item} listRank={i} />;
+        {AlbumDetailsQuery.isSuccess &&
+          AlbumDetailsQuery.data &&
+          AlbumDetailsQuery.data?.tracks.items.map((item, i) => {
+            const songDetails = {
+              title: item.name,
+              image: "",
+              id: item.id,
+              artists: item.artists.map((artist) => artist.name),
+              release_year: AlbumDetailsQuery?.data?.release_date,
+              album: AlbumDetailsQuery.data.name,
+              url: item.preview_url,
+            };
+
+            return <VerticalSongCard key={i} songDetails={songDetails} listRank={i} />;
           })}
-      </Box> */}
+      </Box>
     </Box>
   );
 };
--- src/pages/SearchResultPage.tsx
+++ src/pages/SearchResultPage.tsx
@@ -2,37 +2,9 @@ import { useSearchParams } from "react-router-dom";
 import { data } from "@components/Header/data";
 import { SearchResultSection } from "@components/SearchResultSection/SearchResultSection";
 import { Box } from "@mui/material";
-import { getReleaseYearValue } from "@hooks/getReleaseYearValue";
 import { SearchFilterTabs } from "@components/SearchFilterTabs/SearchFIlterTabs";
 import { useEffect } from "react";
-
-const getFilterDataArray = (valueArray) => {
-  const valueFilter = valueArray.map((details) => {
-    const imageUrl = details.images ? details?.images[0]?.url : details?.album.images[0]?.url;
-    const artists = details.artists ? details.artists.map((item) => item.name) : [details?.type];
-    const release_year = details.album
-      ? getReleaseYearValue(details.album.release_date)
-      : details.release_date
-      ? getReleaseYearValue(details.release_date)
-      : "";
-    const albumName = details.album ? details.album.name : "";
-    const previewUrl = "";
-
-    const filterdDeatils = {
-      title: details?.name,
-      id: details.id,
-      artists,
-      release_year,
-      albumName,
-      image: imageUrl ? imageUrl : "",
-      ...(previewUrl ? { previewUrl } : {}),
-    };
-
-    return filterdDeatils;
-  });
-
-  return valueFilter;
-};
+import { getSongsFilteredData } from "@utils/getSongsFilteredData";
 
 export const SearchResultPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -52,21 +24,21 @@ export const SearchResultPage = () => {
       <Box>
         <SearchResultSection
           searchCategoryTitle="Albums"
-          songDeatils={getFilterDataArray(searchData.albums.items)}
+          songDeatils={getSongsFilteredData(searchData.albums.items)}
         />
         <SearchResultSection
           searchCategoryTitle="Tracks"
-          songDeatils={getFilterDataArray(searchData.tracks.items)}
+          songDeatils={getSongsFilteredData(searchData.tracks.items)}
           cardType={"Tracks"}
         />
         <SearchResultSection
           searchCategoryTitle="Playlists"
-          songDeatils={getFilterDataArray(searchData.playlists.items)}
+          songDeatils={getSongsFilteredData(searchData.playlists.items)}
           cardType={"Playlists"}
         />
         <SearchResultSection
           searchCategoryTitle="Artists"
-          songDeatils={getFilterDataArray(searchData.artists.items)}
+          songDeatils={getSongsFilteredData(searchData.artists.items)}
         />
       </Box>
     </div>
