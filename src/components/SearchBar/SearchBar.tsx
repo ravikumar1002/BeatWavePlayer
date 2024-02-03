687747305b93725c717b18885f4b2ee65a36017f
Ravi kumar <1002kumarravi@gmail.com>
2024-02-03 15:47:07 +0530

    refactor: separate function from jsx

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -21,6 +21,27 @@ export const SearchBar = (props: ISearchBarProps) => {
     showSearchSuggestion,
   } = useSearchBar();
 
+  const inputChangeHandler = (e) => {
+    showSuggestionFn(e.target.value.trim().length);
+    setSearchString(e.target.value);
+  };
+
+  const inputKeyDownHandler = (e) => {
+    if (e.key === "Enter" && e.target.value.length > 0) {
+      navigate(`/search?q=${searchString}`);
+      setShowSearchSuggestion(false);
+    }
+  };
+
+  const inputFocusHandler = (e) => {
+    showSuggestionFn(e.target.value.trim().length);
+  };
+
+  const closeButtonHandler = () => {
+    setSearchString("");
+    showSuggestionFn(0);
+  };
+
   return (
     <Box ref={searchSuggestionRef}>
       <Paper
@@ -32,31 +53,13 @@ export const SearchBar = (props: ISearchBarProps) => {
           sx={{ ml: 1, flex: 1 }}
           placeholder="Search"
           inputProps={{ "aria-label": "search" }}
-          onChange={(e) => {
-            showSuggestionFn(e.target.value.trim().length);
-            setSearchString(e.target.value);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && e.target.value.length > 0) {
-              navigate(`/search?q=${searchString}`);
-              setShowSearchSuggestion(false);
-            }
-          }}
+          onChange={inputChangeHandler}
+          onKeyDown={inputKeyDownHandler}
           value={searchString}
-          onFocus={(e) => {
-            showSuggestionFn(e.target.value.trim().length);
-          }}
+          onFocus={inputFocusHandler}
         />
         {searchString.length > 0 && (
-          <IconButton
-            type="button"
-            className="p-3"
-            aria-label="clear"
-            onClick={() => {
-              setSearchString("");
-              showSuggestionFn(0);
-            }}
-          >
+          <IconButton type="button" className="p-3" aria-label="clear" onClick={closeButtonHandler}>
             <CloseIcon />
           </IconButton>
         )}
