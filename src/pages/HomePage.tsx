2d03a1476fe6017133b929c78d1adf1fdb49b8ce
Ravi kumar <1002kumarravi@gmail.com>
2024-02-04 11:14:11 +0530

    commit currently unused file

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,33 +1,4 @@
-import { useEffect, useState } from "react";
-
-import { CategorySection, SkeletonCategoryCard } from "@components/CategorySection";
-import { CategoryDTO } from "@dto/categoryDTO";
-import { GetSpotifyDataAsJSON } from "@services/getApiData";
-
-// const loadingStateKey = {
-//     loading: "loading",
-//     fulfilled: "fulfilled",
-//     default: "default",
-// }
-
 const HomePage = () => {
-  // const [data, setData] = useState<CategoryDTO>()
-  // const [loadingState, setLoadingState] = useState<"loading" | "fulfilled" | "default">("default")
-
-  // const getTrendingData = async () => {
-  //     setLoadingState("loading")
-  //     const trendingResponse = await GetSpotifyDataAsJSON<CategoryDTO>("browse/featured-playlists?limit=24", {
-  //         params: {},
-  //     });
-  //     setData(trendingResponse)
-  //     setLoadingState("fulfilled")
-  //     return trendingResponse
-  // }
-
-  // useEffect(() => {
-  //     getTrendingData()
-  // }, [])
-
   return <div></div>;
 };
 
