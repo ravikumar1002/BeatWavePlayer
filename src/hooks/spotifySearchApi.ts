35c1ef8b46fc61d6b6e312e8cd407cdca0547fe5
Ravi kumar <1002kumarravi@gmail.com>
2024-02-07 10:51:10 +0530

    remove unwnted code

--- src/hooks/spotifySearchApi.ts
+++ src/hooks/spotifySearchApi.ts
@@ -1,9 +1,7 @@
 import { GetSpotifyDataAsJSON } from "../services/getApiData";
 
 export const spotifySearchApi = async (searchString: string, limit: number) => {
-    const trendingResponse = await GetSpotifyDataAsJSON(`/search?q=${searchString}&type=album%2Cartist%2Cplaylist%2Ctrack&limit=${limit}`, {
-        params: {},
-    });
+    const trendingResponse = await GetSpotifyDataAsJSON(`/search?q=${searchString}&type=album%2Cartist%2Cplaylist%2Ctrack&limit=${limit}`);
     return trendingResponse
 }
 
